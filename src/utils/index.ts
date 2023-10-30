74dcd5f4ee479fa325f1fab1863d2dd16973e27d
zlq <midhuhu@163.com>
2023-10-30 11:26:32 +0800

    [mod] update

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -31,5 +31,26 @@ export default <Utils>{
         const isWin = u.indexOf('Win') !== -1
         const isMac = u.indexOf('Mac') !== -1
         return isIOS ? 1 : isAndroid ? 2 : isWin ? 3 : isMac ? 4 : 0
-    }
+    },
+    /**
+     * md5格式(32位)
+     * @param value 
+     */
+    isMd5(value: string): boolean {
+        return /^[a-fA-F0-9]{32}$/.test(value)
+    },
+    /**
+     * 版本号(version)格式必须为X.Y.Z
+     * @param value 
+     */
+    isVersion(value: string): boolean {
+        return /^\d+(?:\.\d+){2}$/.test(value)
+    },
+    /**
+     * 密码强度校验， 最少6位， 包括至少1个大写字母， 1个小写字母，1个数字，1个特殊字符
+     * @param value 
+     */
+    isComplexPassword(value: string): boolean {
+        return /^\S*(?=\S{6,})(?=\S*\d)(?=\S*[A-Z])(?=\S*[a-z])(?=\S*[!@#$%^&*? ])\S*$/.test(value)
+    },
 }
--- src/utils/type.ts
+++ src/utils/type.ts
@@ -1,4 +1,7 @@
 export interface Utils {
     getType(value: unknown): string
     getOsType(): number
+    isMd5(value: string): boolean
+    isVersion(value: string): boolean
+    isComplexPassword(value: string): boolean
 } 
